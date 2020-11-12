7c6ac56028a1f2b6e5fa2819d20c42791bc11cac
Sushma <sushmaskashyaphw1999@gmail.com>
2020-11-12 14:01:44 +0530

    Uodated faculty page

--- PMS/Admin.php
+++ PMS/Admin.php
@@ -18,15 +18,15 @@ if($role=="Admin")
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Bootstrap 4, from LayoutIt!</title>
+    <link rel="icon" type="image/png" href="images/logo.png" type="image/x-icon">
+    <title>Project Management System</title>
 
     <meta name="description" content="Source code generated using layoutit.com">
     <meta name="author" content="LayoutIt!">
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <style type="text/css">
+    <link href="css/styles3.css" rel="stylesheet">
+    <!--<style type="text/css">
       body{
            background: url('https://bmsit.ac.in/public/assets/images/slider/main.jpg'); 
            background-position: center;
@@ -51,7 +51,7 @@ if($role=="Admin")
           text-decoration: underline;
         }
 
-    </style>
+    </style>-->
 
   </head>
 <body>
@@ -142,7 +142,97 @@ if($role=="Admin")
 elseif($role=="Faculty")    
 {
 ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/logo.png" type="image/x-icon">
+    <title>Project Management System</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 topnavbar">
+      <div style="float: left; padding-right: 1%;">
+        <img src="images/logo.png" height="80"/>
+      </div>
+      <br>
+      <div style="float: left;">
+        <a style="font-family: Arial; font-size: 130%; color: #fff; font-weight: 600">BMS</a><br />
+        <a style="font-family: Calibri; color: #fff; font-size: 118%; font-weight: 600">INSTITUTE OF TECHNOLOGY</a>
+      </div>
+      <div class="col-md-12">
+        <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="FACULTY/skill.php">
+            <span class="material-icons hidden-md-down">insights</span><br class="hidden-md-down"> Skill Matrix
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="FACULTY/view.php">
+            <span class="material-icons hidden-md-down">view_module</span><br class="hidden-md-down"> View
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="FACULTY/mail.php">
+            <span class="material-icons hidden-md-down">email</span><br class="hidden-md-down"> Mail
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="FACULTY/meeting.php">
+            <span class="material-icons hidden-md-down">book_online</span><br class="hidden-md-down"> Meeting
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="FACULTY/#">
+            <span class="material-icons hidden-md-down">description</span><br class="hidden-md-down"> Reports
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">
+            <span class="material-icons hidden-md-down">power_settings_new</span><br class="hidden-md-down"> Logout
+          </a>
+        </li>
+      </ul>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+  <div class="row mid-row">
+    <div class="col-md-12 col-lg-12">
+      <div class="display-box">
+        <h2><u>WELCOME</u></h2>
+        <br>
+        <!--<div>-->
+          <span class="material-icons">person</span>
+        <!--</div>-->
+        <div class="message">
+        <label style="float: center;">
+          <?php
+            print $role;
+            echo "<br/>";
+            print $user;
+          ?>
+        </label>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
+    <!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -170,9 +260,9 @@ elseif($role=="Faculty")
     <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
     <th width="140" scope="col"><font color="White" size="5">
     <?php
-    print $role;
-    echo "<br/>";
-    print $user;
+    //print $role;
+    //echo "<br/>";
+    //print $user;
     ?>
         </font></th>
     <th width="63" scope="col">&nbsp;</th>
@@ -192,7 +282,7 @@ elseif($role=="Faculty")
        <tr>
         <td colspan="10"><img src="home.png" height="50%" width="100%"></img></td>
     </tr>
-</table>
+</table>-->
  <?php
 }
 else   

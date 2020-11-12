1aff92cb64255ebded779f4bc14d27b18d6d4521
Sneha <snehamj2@gmail.com>
2020-11-12 19:11:45 +0530

    Updated student page

--- PMS/Admin.php
+++ PMS/Admin.php
@@ -137,6 +137,7 @@ if($role=="Admin")
   </body>
 </html>
    
+
  <?php
 }
 elseif($role=="Faculty")    
@@ -288,7 +289,88 @@ elseif($role=="Faculty")
 else   
 {
 ?>
-      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/logo.png" type="image/x-icon">
+    <title>Project Management System</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12 topnavbar">
+      <div style="float: left; padding-right: 1%;">
+        <img src="images/logo.png" height="80"/>
+      </div>
+      <br>
+      <div style="float: left;">
+        <a style="font-family: Arial; font-size: 130%; color: #fff; font-weight: 600">BMS</a><br />
+        <a style="font-family: Calibri; color: #fff; font-size: 118%; font-weight: 600">INSTITUTE OF TECHNOLOGY</a>
+      </div>
+      <div class="col-md-12">
+        <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="STUDENT/project.php">
+            <span class="material-icons hidden-md-down">article</span><br class="hidden-md-down"> Project
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="STUDENT/skill.php">
+            <span class="material-icons hidden-md-down">insights</span><br class="hidden-md-down">View Skill Matrix
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="STUDENT/mail.php">
+            <span class="material-icons hidden-md-down">email</span><br class="hidden-md-down"> Mail
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">
+            <span class="material-icons hidden-md-down">power_settings_new</span><br class="hidden-md-down"> Logout
+          </a>
+        </li>
+      </ul>
+      </div>
+    </div>
+    </div>
+    </div>
+  <div class="container-fluid">
+  <div class="row mid-row">
+    <div class="col-md-12 col-lg-12">
+      <div class="display-box">
+        <h2><u>WELCOME</u></h2>
+        <br>
+        <!--<div>-->
+          <span class="material-icons">person</span>
+        <!--</div>-->
+        <div class="message">
+        <label style="float: center;">
+           <?php
+            print $role;
+            echo "<br/>";
+            print $user;
+          ?> 
+        </label>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
+      <!-- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -317,9 +399,9 @@ else
     <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
     <th width="140" scope="col"><font color="White" size="5">
 	<?php
-    print $role;
-    echo "<br/>";
-    print $user;
+   // print $role;
+    //echo "<br/>";
+   // print $user;
     ?></font></th>
     <th width="63" scope="col">&nbsp;</th>
   </tr>
@@ -343,13 +425,11 @@ else
 <?php
 }
 ?>
-</table>
+</table> -->
 <p>
   <?php
 }
-?>
-    
-    
+?>    
 </p>
 <p>&nbsp;</p>
   </table>
